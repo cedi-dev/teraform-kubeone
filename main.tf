7943f1cff2bcd637fd022054120e671e5dc2bee8
Cedric Kienzler <cedric.kienzler@innovo-cloud.de>
2021-06-06 22:33:08 +0200

    make controlplane configurable

--- main.tf
+++ main.tf
@@ -32,13 +32,13 @@ resource "hcloud_network_subnet" "kubeone" {
 }
 
 resource "hcloud_server_network" "control_plane" {
-  count     = 3
+  count     = control_plane_replicas
   server_id = element(hcloud_server.control_plane.*.id, count.index)
   subnet_id = hcloud_network_subnet.kubeone.id
 }
 
 resource "hcloud_server" "control_plane" {
-  count       = 3
+  count       = var.control_plane_replicas
   name        = "${var.cluster_name}-control-plane-${count.index + 1}"
   server_type = var.control_plane_type
   image       = var.image
