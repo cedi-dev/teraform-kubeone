5d8b3b7e1bcc08836cb4bc31cc23be894bf00d96
Cedric Kienzler <github@cedric-kienzler.de>
2022-07-07 22:40:53 +0200

    Revert "change api endpoint name to something more meaningful"
    This reverts commit e814ae4385773c0a462249e95bb88dc5caa1ee59.

--- dns.tf
+++ dns.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "dns_api_v4" {
   zone_id = var.cloudflare_zone_id
-  name    = "k8s"
+  name    = "api"
   value   = hcloud_load_balancer.load_balancer.ipv4
   type    = "A"
   ttl     = 1
@@ -9,7 +9,7 @@ resource "cloudflare_record" "dns_api_v4" {
 
 resource "cloudflare_record" "dns_api_v6" {
   zone_id = var.cloudflare_zone_id
-  name    = "k8s"
+  name    = "api"
   value   = hcloud_load_balancer.load_balancer.ipv6
   type    = "AAAA"
   ttl     = 1
